771192b847de79dbb78be4918ab3bcf9a1f02dc6
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-03-26 18:56:01 +0300

    Модификация справки

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .usage('[options] <filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2, options) => {
     console.log(`Comparing files:
 File 1: ${filepath1}
