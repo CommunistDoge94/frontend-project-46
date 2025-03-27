e6b583c1aeaa62a087294ddb0e48d25c22378c78
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-03-27 21:22:55 +0300

    Вынос парсинга в модуль parsers

--- src/parsing.js
+++ src/parsing.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import yaml from 'js-yaml';
+import parse from './parsers.js';
 
 export const parseFile = (filepath) => {
   const absolutePath = path.resolve(process.cwd(), filepath);
@@ -12,17 +12,5 @@ export const parseFile = (filepath) => {
   const content = fs.readFileSync(absolutePath, 'utf-8');
   const extension = path.extname(filepath).slice(1).toLowerCase();
 
-  try {
-    switch (extension) {
-      case 'json':
-        return JSON.parse(content);
-      case 'yaml':
-      case 'yml':
-        return yaml.load(content);
-      default:
-        throw new Error(`Unsupported format: ${extension}`);
-    }
-  } catch (e) {
-    throw new Error(`Error parsing ${filepath}: ${e.message}`);
-  }
+  return parse(content, extension);
 };
