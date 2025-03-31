98c85bb5ebd89fb9c194f45f1c0b05c1f7cabf60
Dmitriy Glebov <dmitriy.glebov94@gmail.com>
2025-04-01 00:14:32 +0300

    Добавление тестов для json-формата

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,11 +5,11 @@ import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
 
 const expectedStylish = readFileSync(getFixturePath('result-stylish.txt'), 'utf-8').trim();
 const expectedPlain = readFileSync(getFixturePath('result-plain.txt'), 'utf-8').trim();
+const expectedJson = readFileSync(getFixturePath('result-json.txt'), 'utf-8').trim();
 
 describe('Stylish format', () => {
   test('JSON files', () => {
@@ -36,5 +36,19 @@ describe('Plain format', () => {
     const file1 = getFixturePath('file1.yml');
     const file2 = getFixturePath('file2.yml');
     expect(genDiff(file1, file2, 'plain')).toEqual(expectedPlain);
+  });
+});
+
+describe('JSON format', () => {
+  test('JSON files', () => {
+    const file1 = getFixturePath('file1.json');
+    const file2 = getFixturePath('file2.json');
+    expect(genDiff(file1, file2, 'json')).toEqual(expectedJson);
+  });
+
+  test('YAML files', () => {
+    const file1 = getFixturePath('file1.yml');
+    const file2 = getFixturePath('file2.yml');
+    expect(genDiff(file1, file2, 'json')).toEqual(expectedJson);
   });
 });
